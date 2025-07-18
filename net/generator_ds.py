0128f74842f9deda2b02ef80610ebcf34a6c3cf5
Loren Nuyts <loren@ysgarden.com>
2025-07-18 13:55:15 +0200

    .

--- net/generator_ds.py
+++ net/generator_ds.py
@@ -42,6 +42,15 @@ class SequentialGenerator(keras.utils.Sequence):
 
         rec_data = Data.loadData(config.data_path, recs[prev_rec], included_channels=self.channels)
         rec_data.apply_preprocess(config)
+        if set(rec_data.channels) != set(self.channels):
+            rec_data.channels = switch_channels(self.channels, rec_data.channels, Nodes.switchable_nodes)
+        if rec_data.channels != self.channels:
+            rec_data.reorder_channels(self.channels)
+
+        if rec_data.channels != self.channels and len(self.channels) != 0:
+            print("Rec channels: ", rec_data.channels)
+            print("self.channels: ", self.channels)
+        assert rec_data.channels == self.channels
         # self.channels = rec_data.channels
         self.data_segs = np.empty(shape=[len(segments), int(config.frame * config.fs), len(self.channels)],
                                   dtype=np.float32)
