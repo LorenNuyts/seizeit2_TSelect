c7c8e4849726e9c314741dc440795a3e2c97cd0f
Loren Nuyts <loren@ysgarden.com>
2025-08-20 18:47:17 +0200

    update before presentation

--- analysis/channel_analysis/__init__.py
+++ analysis/channel_analysis/__init__.py
@@ -1,15 +1,16 @@
 import itertools
 import os
 from collections import defaultdict
-from typing import List
+from typing import List, Set, Dict
 
 import numpy as np
 import pandas as pd
 from matplotlib import pyplot as plt
+from openpyxl.styles import Font, Alignment
+from openpyxl.utils import get_column_letter
 
 from net.DL_config import Config
-from utility.paths import get_path_config, get_path_results
-from utility.stats import Results
+from utility.paths import get_path_config
 
 
 def count_selected_channels_across_folds(base_dir, configs: List[Config], output_path: str = None):
@@ -89,7 +90,8 @@ def mine_frequent_channels(base_dir, configs: List[Config], output_path: str = N
                 f.write('\n')
             rules.to_csv(output_file, index=False, mode='a')
 
-def find_interchangeable_channels(base_dir, configs: List[Config], output_path:str):
+def find_interchangeable_channels(base_dir, configs: List[Config], output_path:str, minimal_support: float = 0.5):
+    assert 0 <= minimal_support <= 1
     for i, config in enumerate(configs):
         config_path = os.path.join(base_dir, "..", "..", get_path_config(config, config.get_name()))
         if os.path.exists(config_path):
@@ -97,6 +99,8 @@ def find_interchangeable_channels(base_dir, configs: List[Config], output_path:s
         else:
             print(f"Config not found for {config.get_name()}")
             continue
+
+        config.nb_folds = len(config.folds)
         # results = []
         # results_path = os.path.join(base_dir, "..", "..", get_path_results(config, config.get_name()))
         # results.append(Results(config))
@@ -113,6 +117,7 @@ def find_interchangeable_channels(base_dir, configs: List[Config], output_path:s
         clusters = [config.channel_selector[fold_i].clusters for fold_i in range(config.nb_folds)]
         # Count how many times two channels appears in the same cluster
         co_occurrence_matrix = np.zeros((len(included_channels), len(included_channels)))
+        nb_folds_channel_selected = np.zeros((len(included_channels), len(included_channels)), dtype=int)
         for clustering_i in clusters:
             for cluster in clustering_i:
                 for ch1, ch2 in itertools.combinations(cluster, 2):
@@ -121,9 +126,22 @@ def find_interchangeable_channels(base_dir, configs: List[Config], output_path:s
 
                 for ch in cluster:
                     co_occurrence_matrix[ch, ch] += 1
+                    nb_folds_channel_selected[ch, ch] += 1
 
-        # Normalize the co-occurrence matrix by the number of folds
-        co_occurrence_matrix /= config.nb_folds
+            channels_in_all_clusters = list(itertools.chain.from_iterable(clustering_i))
+            for ch1, ch2 in itertools.combinations(channels_in_all_clusters, 2):
+                nb_folds_channel_selected[ch1, ch2] += 1
+                nb_folds_channel_selected[ch2, ch1] += 1
+
+        # Normalize the co-occurrence matrix by the  number of folds each channel got past the irrelevant selector
+        for m in range(co_occurrence_matrix.shape[0]):
+            for n in range(m, co_occurrence_matrix.shape[1]):
+                co_occurrence_matrix[m, n] /= nb_folds_channel_selected[m, n]
+                if m != n:
+                    co_occurrence_matrix[n, m] /= nb_folds_channel_selected[m, n]
+                    assert co_occurrence_matrix[m, n] == co_occurrence_matrix[n, m] or \
+                    (np.isnan(co_occurrence_matrix[m, n]) and np.isnan(co_occurrence_matrix[n, m]))
+
 
         # Average the evaluation metric for channel selection across the folds to get an indication of how relevant
         # the channel is
@@ -136,32 +154,282 @@ def find_interchangeable_channels(base_dir, configs: List[Config], output_path:s
         # Average the relevance across folds
         average_relevance_channels = {ch: metric / config.nb_folds for ch, metric in average_relevance_channels.items()}
 
-        # Save selected channels, co-occurrence matrix and average_relevance to a CSV file
-        output_file = os.path.join(output_path, f"{config.get_name()}_interchangeable_channels.csv")
-        os.makedirs(os.path.dirname(output_file), exist_ok=True)
-        with pd.ExcelWriter(output_file) as writer:
-            selected_channels_df = pd.DataFrame(selected_channels, index=config.selected_channels.keys(), columns=['Selected Channels'])
-            selected_channels_df.to_excel(writer, sheet_name='Selected Channels')
-            co_occurrence_df = pd.DataFrame(co_occurrence_matrix, index=included_channels, columns=included_channels)
-            co_occurrence_df.to_excel(writer, sheet_name='Co-occurrence Matrix')
-            average_relevance_df = pd.DataFrame(list(average_relevance_channels.values()),
-                                                columns=['Average Relevance'],
-                                                index=included_channels)
-            average_relevance_df.to_excel(writer, sheet_name='Average Relevance')
-            print(f"Interchangeable channels for {config.get_name()} saved to {output_file}")
+        # Select all channels that have a co-occurrence above a certain threshold
+        processed_co_occurrence = process_co_occurrence_matrix(co_occurrence_matrix, minimal_support)
 
+        # Add relevance to results
+        def co_occurrence2str(chs: dict) -> Dict[str, dict | str]:
+            result = {}
+            for key, value in chs.items():
+                chs_relevance = {included_channels[c]: average_relevance_channels[c] for c in key}
+                sorted_chs = [f"{k} ({'%.2f' % v})" for k, v in sorted(chs_relevance.items(), key=lambda item: item[1], reverse=True)]
+                key_str = ', '.join(sorted_chs)
+
+                if value == []:
+                    value_str = ""
+                else:
+                    value_str = co_occurrence2str(value)
+
+                result[key_str] = value_str
+            return result
+
+        processed_co_occurrence_with_relevance = co_occurrence2str(processed_co_occurrence)
+
+
+        # for key, value in processed_co_occurrence.items():
+        #
+        # for c in processed_co_occurrence:
+        #     c_relevance = {included_channels[ch]: average_relevance_channels[ch] for ch in c}
+        #     sorted_c = [f"{k}: {'%.2f' % v}" for k, v in sorted(c_relevance.items(), key=lambda item: item[1], reverse=True)]
+        #     processed_co_occurrence_with_relevance.append(sorted_c)
+
+        # Save selected channels, co-occurrence matrix and average_relevance to a CSV file
+        output_file = os.path.join(output_path, f"{config.get_name()}_interchangeable_channels.xlsx")
+        os.makedirs(os.path.dirname(output_file), exist_ok=True)
+
+        with pd.ExcelWriter(output_file, engine="openpyxl") as writer:
+            workbook = writer.book
+            sheet = workbook.create_sheet("Interchangeable Channels")
+            writer.sheets["Interchangeable Channels"] = sheet
+
+            # Title font and alignment
+            title_font = Font(bold=True, size=12)
+            alignment = Alignment(horizontal="center")
+
+            # Write title and selected channels
+            sheet.cell(row=1, column=1, value="Selected Channels").font = title_font
+            sheet.cell(row=1, column=1).alignment = alignment
+
+            selected_channels_df = pd.DataFrame(
+                selected_channels,
+                index=[f"Fold {i}" for i in config.selected_channels.keys()]
+            )
+            selected_channels_df.to_excel(writer, sheet_name="Interchangeable Channels", startrow=2)
+
+            # Adjust column widths for selected channels
+            for col_num in range(1, len(selected_channels_df.columns) + 1):
+                col_letter = get_column_letter(col_num)
+                sheet.column_dimensions[col_letter].width = 15
+
+            # Write title and processed co-occurrence
+            start_row = 2 + len(selected_channels_df) + 4  # Add extra whitespace
+            sheet.cell(row=start_row, column=1, value="Processed Co-occurrence with Relevance").font = title_font
+            sheet.cell(row=start_row, column=1).alignment = alignment
+
+            start_row += 1
+            # Write each list in processed_co_occurrence_with_relevance as a row
+            def write_co_occurrence_to_sheet(co_occurrence_dict, sheet, start_row, start_column):
+                row_nb = start_row
+                for key, value in co_occurrence_dict.items():
+                    splitted_key = key.split(", ")
+                    for j, v in enumerate(splitted_key, start=start_column):
+                        sheet.cell(row=row_nb, column=j, value=v)
+                    row_nb += 1
+                    if isinstance(value, dict):
+                        # If value is a dict, write it recursively
+                        extra_rows = write_co_occurrence_to_sheet(value, sheet, row_nb, start_column+1)
+                        row_nb += extra_rows
+                return row_nb - start_row
+            start_row += write_co_occurrence_to_sheet(processed_co_occurrence_with_relevance, sheet, start_row, 1)
+
+            # Write title and co-occurrence matrix
+            # start_row += len(processed_co_occurrence_with_relevance) + 4  # Add extra whitespace
+            start_row += 4  # Add extra whitespace
+            sheet.cell(row=start_row, column=1, value="Co-occurrence Matrix").font = title_font
+            sheet.cell(row=start_row, column=1).alignment = alignment
+
+            co_occurrence_df = pd.DataFrame(
+                co_occurrence_matrix,
+                index=included_channels,
+                columns=included_channels
+            )
+            co_occurrence_df.to_excel(writer, sheet_name="Interchangeable Channels", startrow=start_row + 1)
+
+            # Write title and average relevance
+            start_row += len(co_occurrence_df) + 4  # Add extra whitespace
+            sheet.cell(row=start_row, column=1, value="Average Relevance").font = title_font
+            sheet.cell(row=start_row, column=1).alignment = alignment
+
+            average_relevance_df = pd.DataFrame(
+                list(average_relevance_channels.values()),
+                columns=["Average Relevance"],
+                index=included_channels
+            )
+            average_relevance_df.to_excel(writer, sheet_name="Interchangeable Channels", startrow=start_row + 1)
+
+            # Adjust column widths for all dataframes
+            for col_num in range(1, average_relevance_df.shape[1] + 1):
+                col_letter = get_column_letter(col_num)
+                sheet.column_dimensions[col_letter].width = 20
+
+        print(f"Interchangeable channels for {config.get_name()} saved to {output_file}")
         # Combine the count and averaged evaluation metric to find the channels that are equally relevant, but redundant
 
 
-def map_clusters_between_folds(clusters, correlation_threshold=0.7):
-    result = np.ndarray(shape=(len(clusters), len(clusters)))
-    for i in range(len(clusters)):
-        for j in range(i, len(clusters)):
-            # If same fold, map all clusters to themselves
-            if i == j:
-                result[i,j] = {c:c for c in range(len(clusters[i]))}
+def process_co_occurrence_matrix(co_occurrence: np.ndarray, minimal_support=0.5):
+    assert 0 <= minimal_support <= 1
+
+    groups: List[Set] = []
+
+    for i in range(co_occurrence.shape[0]):
+        # Find all indices j where co_occurrence[i, j] >= minimal_support
+        # and j >= i to avoid duplicates
+        group = {i}
+        for j in range(i + 1, co_occurrence.shape[1]):
+            if co_occurrence[i, j] >= minimal_support:
+                group.add(j)
+
+        to_remove = set()
+        for m, n in itertools.combinations(group, 2):
+            if co_occurrence[m, n] < minimal_support:
+                to_remove.add(n)
+        group.difference_update(to_remove)
+
+        if not all(np.isnan(co_occurrence[i])):
+            # Check if the group is already in groups
+            if not any(group.issubset(existing_group) for existing_group in groups):
+                # If not, add it to groups
+                groups.append(group)
+
+    # index_structure = defaultdict(list)
+    # for i, group in enumerate(groups):
+    #     for elem in group:
+    #         index_structure[elem].append(i)
+
+    def list_to_tree(sets: List[Set]):
+        result = dict()
+        sets = sorted(sets, key=len)
+        already_handled = set()
+        for s_i, s in enumerate(sets):
+            if s_i in already_handled:
                 continue
-            # If different folds, map clusters based on correlation
+            common = [sets[k] for k in range(s_i + 1, len(sets)) if len(s.intersection(sets[k])) > 0]
+            if not common:
+                result[frozenset(s)] = []
+            else:
+                intersection = set.intersection(*common, s)
+                if not intersection:
+                    result[frozenset(s)] = []
+                    continue
+                differences = [other.difference(intersection) for other in [s, *common]]
+                to_remove_differences = set()
+                for diff_i, diff in enumerate(differences):
+                    for elem in diff:
+                        others = set.union(*[d for d_i, d in enumerate(differences) if d_i != diff_i])
+                        values = [co_occurrence[elem, o] for o in others if (co_occurrence[elem, o] < minimal_support or
+                                  np.isnan(co_occurrence[elem, o]))]
+                        if all(np.isnan(values)):
+                            intersection.add(elem)
+                            to_remove_differences.add(elem)
+
+                differences = [{d for d in diff if d not in to_remove_differences} for diff in differences]
+                differences = [d for d in differences if len(d) > 0]
+                if not differences:
+                    result[frozenset(intersection)] = []
+                else:
+                    result[frozenset(intersection)] = list_to_tree(differences)
+                already_handled.update({sets.index(c) for c in common})
+
+        return result
+
+    return list_to_tree(groups)
+
+    # def add_to_partial_result(to_add):
+    #     added_at_least_once = False
+    #     maximal_possibilities = []
+    #     results_of = [for g in groups if add_to_result_of in g]
+    #     for result_i in index_structure[add_to_result_of]:
+    #         maximal_possibility = groups[result_i].copy()
+    #         can_add = True
+    #         for elem in groups[result_i]:
+    #             if elem == to_add or elem == add_to_result_of:
+    #                 continue
+    #             # if np.isnan(co_occurrence[elem, to_add]) or co_occurrence[elem, to_add] < minimal_support:
+    #             if co_occurrence[elem, to_add] < minimal_support:
+    #                 can_add = False
+    #                 maximal_possibility.remove(elem)
+    #         if can_add:
+    #             groups[result_i].add(to_add)
+    #             added_at_least_once = True
+    #         else:
+    #             maximal_possibilities.append(maximal_possibility)
+    #     return added_at_least_once, maximal_possibilities
+    #
+    # groups: List[Set] = []
+    #
+    # for i in range(co_occurrence.shape[0]):
+    #     for j in range(i, co_occurrence.shape[1]):
+    #         if co_occurrence[i, j] >= minimal_support:
+    #             added_i, possibilities_i = add_to_partial_result(i, j)
+    #             added_j, possibilities_j = add_to_partial_result(j, i)
+    #             if not added_i and not added_j:
+    #                 if len(possibilities_i) == 0 and len(possibilities_j) == 0:
+    #                     groups.append({i, j})
+    #                 else:
+    #                     for p in possibilities_i:
+    #                         p.add(i)
+    #                         if p not in groups:
+    #                             groups.append(p)
+    #                     for p in possibilities_j:
+    #                         p.add(j)
+    #                         if p not in groups:
+    #                             groups.append(p)
+    #
+    #
+    #
+    # # groups = retain_maximal_sets(groups) # TODO adapt indexing structure!
+    # # result = []
+    # # for i, indices in index_structure.items():
+    # #     if len(indices) > 1:
+    # #         results_i = [groups[idx] for idx in indices]
+    # #         tree = sets_to_tree(results_i)
+    # #     else:
+    # #         result.append(groups[indices.pop()])
+    #
+    # return groups
+
+# def retain_maximal_sets(sets):
+#     # Sort sets by length in descending order
+#     sorted_sets = sorted(sets, key=len, reverse=True)
+#     result = []
+#
+#     for s in sorted_sets:
+#         # Add the set to the result only if it is not a subset of any set already in the result
+#         if not any(s < other for other in result):
+#             result.append(s)
+#
+#     return result
+#
+# def sets_to_tree(sets_):
+#     from collections import defaultdict
+#
+#     def build_tree(sets):
+#         if not sets:
+#             return None
+#
+#         # Convert sets to sorted lists for consistent processing
+#         sorted_lists = [sorted(s) for s in sets]
+#
+#         # Find the common elements
+#         common_elements = set.intersection(*sets) if sets else set()
+#
+#         # Remove the common elements from all sets
+#         children = defaultdict(list)
+#         for s in sets:
+#             remaining = s - common_elements
+#             if remaining:
+#                 children[frozenset(remaining)].append(remaining)
+#
+#         # Recursively build the tree for each child
+#         return {
+#             "value": list(common_elements),
+#             "children": [build_tree([set(c) for c in child]) for child in children.values()]
+#         }
+#
+#     return build_tree(sets_)
+
+# def co_occurrence_matrix2tree(co_occur)
+
 
 
 
--- analysis/excel.py
+++ analysis/excel.py
@@ -4,7 +4,7 @@ from typing import List
 
 import pandas as pd
 
-from utility.constants import evaluation_metrics
+from utility.constants import evaluation_metrics, parse_location, Locations, Keys
 from net.DL_config import get_base_config, get_channel_selection_config
 from utility.paths import get_path_results
 from utility.stats import Results
@@ -59,32 +59,72 @@ def print_table(configs: list, metrics: List[str], output_path: str):
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument("--suffix", type=str, nargs="?", default="")
+    parser.add_argument(
+        '--locations',
+        nargs='+',  # accept multiple inputs
+        type=parse_location,
+        default=[parse_location(l) for l in Locations.all_keys()],
+        help=f"List of locations. Choose from: {', '.join(Locations.all_keys())}. "
+             f"Defaults to [{Locations.all_keys()}]."
+    )
     args = parser.parse_args()
-    suffix_ = args.suffix
-    configs_base = [
-        get_base_config(base_dir, suffix=suffix_),
-                get_channel_selection_config(base_dir, suffix=suffix_),
-                get_channel_selection_config(base_dir, suffix=suffix_, evaluation_metric=evaluation_metrics['score']),]
-    configs_wearables = [
-        get_base_config(base_dir, suffix=suffix_, included_channels='wearables'),
-                get_channel_selection_config(base_dir, suffix=suffix_, included_channels='wearables'),
-                get_channel_selection_config(base_dir, suffix=suffix_, evaluation_metric=evaluation_metrics['score'],
-                                                included_channels='wearables'),]
-    metrics_ = ['average_nb_channels', 'average_selection_time', 'average_train_time', 'average_total_time',
-        'average_f1_ovlp_best_threshold', 'average_fah_ovlp_best_threshold', 'average_fah_epoch_best_threshold',
-                'average_prec_ovlp_best_threshold',
-                'average_sens_ovlp_best_threshold', 'average_rocauc_best_threshold', 'average_score_best_threshold',
-                # 'best_average_f1_ovlp', 'best_average_fah_ovlp', 'best_average_prec_ovlp', 'best_average_sens_ovlp',
-                # 'best_average_rocauc', 'best_average_score',
 
-                'average_f1_ovlp_th05', 'average_fah_ovlp_th05', 'average_fah_epoch_th05',
-                'average_prec_ovlp_th05', 'average_sens_ovlp_th05', 'average_rocauc_th05', 'average_score_th05',
-                'median_f1_ovlp_best_threshold', 'median_fah_ovlp_best_threshold', 'median_fah_epoch_best_threshold',
-                'median_prec_ovlp_best_threshold',
-                'median_sens_ovlp_best_threshold', 'median_rocauc_best_threshold', 'median_score_best_threshold',
-                'median_f1_ovlp_th05', 'median_fah_ovlp_th05', 'median_fah_epoch_th05',
-                'median_prec_ovlp_th05', 'median_sens_ovlp_th05',
-                'median_rocauc_th05', 'median_score_th05'
+    locations_ = sorted(list(dict.fromkeys(args.locations)))
+    suffix_ = args.suffix
+    configs_ = [
+        # get_base_config(base_dir, locations=locations_, suffix=suffix_, CV=Keys.stratified,
+        #                 held_out_fold=True, pretty_name="Base"),
+        # get_channel_selection_config(base_dir, locations=locations_, suffix=suffix_,
+        #                              evaluation_metric=evaluation_metrics['score'], CV=Keys.stratified,
+        #                              held_out_fold=True, pretty_name="Channel Selection (th=-100)"),
+        # get_channel_selection_config(base_dir, locations=locations_, suffix=suffix_,
+        #                              evaluation_metric=evaluation_metrics['score'],
+        #                              irrelevant_selector_threshold=0.5, CV=Keys.stratified,
+        #                              held_out_fold=True, pretty_name="Channel Selection (th=0.5)"),
+        # get_base_config(base_dir, unique_locations, suffix=suffix_, CV=Keys.leave_one_hospital_out,
+        #                 held_out_fold=True, pretty_name="Baseline"),
+        get_channel_selection_config(base_dir, locations=locations_, suffix=suffix_,
+                                     evaluation_metric=evaluation_metrics['score'], CV=Keys.leave_one_hospital_out,
+                                     held_out_fold=True, pretty_name="Channel Selection (th=-100)"),
+        get_channel_selection_config(base_dir, locations=locations_, suffix=suffix_,
+                                     evaluation_metric=evaluation_metrics['score'],
+                                     irrelevant_selector_threshold=0.5, CV=Keys.leave_one_hospital_out,
+                                     held_out_fold=True, pretty_name="Channel Selection (th=0.5)"),
+        # get_channel_selection_config(base_dir, locations=args.locations, suffix=suffix_),
+        # get_channel_selection_config(base_dir, locations=args.locations, suffix=suffix_,
+        #                              included_channels='wearables'),
+        # get_channel_selection_config(base_dir, locations=locations_, suffix=suffix_,
+        #                              evaluation_metric=evaluation_metrics['score'],
+        #                              irrelevant_selector_threshold=0.5),
+        # get_channel_selection_config(base_dir, locations=locations_, suffix=suffix_,
+        #                              evaluation_metric=evaluation_metrics['score'],
+        #                              irrelevant_selector_threshold=0.5, CV=Keys.leave_one_hospital_out),
+        # get_channel_selection_config(base_dir, locations=args.locations, suffix=suffix_,
+        #                              included_channels='wearables',
+        #                              evaluation_metric=evaluation_metrics['score'])
+    ]
+    # configs_base = [
+    #     get_base_config(base_dir, suffix=suffix_),
+    #             get_channel_selection_config(base_dir, suffix=suffix_),
+    #             get_channel_selection_config(base_dir, suffix=suffix_, evaluation_metric=evaluation_metrics['score']),]
+    # configs_wearables = [
+    #     get_base_config(base_dir, suffix=suffix_, included_channels='wearables'),
+    #             get_channel_selection_config(base_dir, suffix=suffix_, included_channels='wearables'),
+    #             get_channel_selection_config(base_dir, suffix=suffix_, evaluation_metric=evaluation_metrics['score'],
+    #                                             included_channels='wearables'),]
+    metrics_ = ['average_nb_channels', 'average_selection_time', 'average_train_time', 'average_total_time',
+        # 'average_f1_ovlp_best_threshold', 'average_fah_ovlp_best_threshold', 'average_fah_epoch_best_threshold',
+        #         'average_prec_ovlp_best_threshold',
+        #         'average_sens_ovlp_best_threshold', 'average_rocauc_best_threshold', 'average_score_best_threshold',
+
+                # 'average_f1_ovlp_th05', 'average_fah_ovlp_th05', 'average_fah_epoch_th05',
+                # 'average_prec_ovlp_th05', 'average_sens_ovlp_th05', 'average_rocauc_th05', 'average_score_th05',
+                # 'median_f1_ovlp_best_threshold', 'median_fah_ovlp_best_threshold', 'median_fah_epoch_best_threshold',
+                # 'median_prec_ovlp_best_threshold',
+                # 'median_sens_ovlp_best_threshold', 'median_rocauc_best_threshold', 'median_score_best_threshold',
+                # 'median_f1_ovlp_th05', 'median_fah_ovlp_th05', 'median_fah_epoch_th05',
+                # 'median_prec_ovlp_th05', 'median_sens_ovlp_th05',
+                # 'median_rocauc_th05', 'median_score_th05'
                 ]
     # metrics_ = ['average_nb_channels', 'average_total_time',
     #             'median_score_th05', 'median_sens_ovlp_th05', 'median_fah_epoch_th05', 'median_fah_ovlp_th05',
@@ -101,10 +141,10 @@ if __name__ == '__main__':
         os.makedirs(os.path.dirname(output_path_))
 
     print("Base table: ")
-    print_table(configs_base, metrics_, output_path_)
-    print("########################################################\n")
-    print("Wearables table: ")
-    print_table(configs_wearables, metrics_, output_path_)
+    print_table(configs_, metrics_, output_path_)
+    # print("########################################################\n")
+    # print("Wearables table: ")
+    # print_table(configs_wearables, metrics_, output_path_)
 
 
 
