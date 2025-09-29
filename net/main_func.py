f11cf4ed1229d0fd106931e67fe91ca00d53e3a6
Loren Nuyts <loren@ysgarden.com>
2025-09-29 11:16:26 +0200

    ..or not, back to debugging

--- net/main_func.py
+++ net/main_func.py
@@ -82,6 +82,8 @@ def train(config, results, load_segments, save_segments, fold=None):
         gc.collect()
         model_save_path = get_path_model(config, name, fold_i)
         path_last_epoch_callback = os.path.join(model_save_path, 'Callbacks', name + f'_{config.nb_epochs:02d}.weights.h5')
+        print(os.path.join(model_save_path, 'MiniRocketLR_model.joblib'))
+        print(os.path.exists(os.path.join(model_save_path, 'MiniRocketLR_model.joblib')))
         if os.path.exists(model_save_path) and os.path.exists(path_last_epoch_callback):
             print('    | Model of fold {} already exists'.format(fold_i))
             continue
