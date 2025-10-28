84314022fab9a484eb41938d4fd26fb5db34c136
Loren Nuyts <loren@ysgarden.com>
2025-10-28 16:22:59 +0100

    dont show which segments differ - computation is too long

--- analysis/debug.py
+++ analysis/debug.py
@@ -259,9 +259,9 @@ def same_segments():
         rep2 = {a.tobytes() for a in old_train_segments}
         if rep1 != rep2:
             print(f"Train segments differ in fold {fold_i}")
-            unmatched_new, unmatched_old = diff_arrays(new_train_segments, old_train_segments)
-            print("     | In new only:", unmatched_new)
-            print("     | In old only:", unmatched_old)
+            # unmatched_new, unmatched_old = diff_arrays(new_train_segments, old_train_segments)
+            # print("     | In new only:", unmatched_new)
+            # print("     | In old only:", unmatched_old)
 
         new_path_segments_val = get_paths_segments_val(new_config, new_config.get_name(), fold_i)
         with open(new_path_segments_val, 'rb') as inp:
