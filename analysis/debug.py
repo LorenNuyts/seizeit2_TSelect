674d2a6ff36ef0dfdf0a55cb77f9762143f4cc15
Loren Nuyts <loren@ysgarden.com>
2025-10-28 12:21:38 +0100

    check segments bug update

--- analysis/debug.py
+++ analysis/debug.py
@@ -234,13 +234,14 @@ def same_segments():
     for fold_i in folds:
         new_path_segments_train = get_paths_segments_train(new_config, new_config.get_name(), fold_i)
         with open(new_path_segments_train, 'rb') as inp:
-            new_train_segments = pickle.load(inp)
+            new_train_segments: List[list] = pickle.load(inp)
 
         old_path_segments_train = get_paths_segments_train(old_config, old_config.get_name(), fold_i)
         with open(old_path_segments_train, 'rb') as inp:
-            old_train_segments = pickle.load(inp)
+            old_train_segments: List[list] = pickle.load(inp)
 
-        assert sorted(new_train_segments) == sorted(old_train_segments), f"Train segments differ in fold {fold_i}"
+        print(f"Fold {fold_i}: New train segments: {len(new_train_segments)}, Old train segments: {len(old_train_segments)}")
+        assert all(sorted(new_train_segments) == sorted(old_train_segments)), f"Train segments differ in fold {fold_i}"
 
         new_path_segments_val = get_paths_segments_val(new_config, new_config.get_name(), fold_i)
         with open(new_path_segments_val, 'rb') as inp:
