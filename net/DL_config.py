bde69ab4ae7316583d6fda67e73ec394c4abc346
Loren Nuyts <loren@ysgarden.com>
2025-12-07 14:19:53 +0100

    remove spaces in suffix

--- net/DL_config.py
+++ net/DL_config.py
@@ -166,10 +166,10 @@ def get_base_config(base_dir, locations, model="ChronoNet", batch_size=128,
         suffix = "CROSStop" + ("__" if len(suffix) != 0 else "") + suffix
     elif included_channels.startswith("[") and included_channels.endswith("]"):
         # Parse the string representation of the list
-        included_channels_str = included_channels[1:-1].replace(",", "_")
+        included_channels_str = included_channels[1:-1].replace(",", "_").strip()
         included_channels = included_channels.strip("[]").split(",")
         included_channels = [ch.strip().strip("'").strip('"') for ch in included_channels]
-        
+
         # Replace CROSStop, BTEleft, BTEright with their respective names
         included_channels = [Nodes.CROSStop if ch == "CROSStop" else ch for ch in included_channels]
         included_channels = [Nodes.BTEleft if ch == "BTEleft" else ch for ch in included_channels]
